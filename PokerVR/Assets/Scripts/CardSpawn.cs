951bd6bd1da27738b1a4b75a8f912d8b8d04e1f3
Rafael Ismael <rafaelmismael@gmail.com>
2017-09-05 09:46:29 -0300

    Logica inical, bater cartas

--- PokerVR/Assets/Scripts/CardSpawn.cs
+++ PokerVR/Assets/Scripts/CardSpawn.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class CardSpawn : MonoBehaviour {
     public GameObject[] spawnees;
+	public Text winPhrase;
+	public GameObject[] spawned = new GameObject[2];
     public Transform spawnPos1;
     public Transform spawnPos2;
-    public Transform spawnPos3;
     private SteamVR_TrackedObject trackedObj;  // referência para o controle
 
     int randomInt;
+	int cardPlayerNumber;
+	int cardDealerNumber;
 
 
     private SteamVR_Controller.Device Controller
@@ -17,24 +22,41 @@ public class CardSpawn : MonoBehaviour {
         get { return SteamVR_Controller.Input((int)trackedObj.index); }
     }
 
-    void Awake()
+    void Start()
     {                         // recupera referência para o controle
         trackedObj = GetComponent<SteamVR_TrackedObject>();
     }
 
-    void Start()
+    void Update()
     {
-        SpawnCards();
+		if (Controller.GetHairTriggerDown ()) {
+			for (int i = 0; i < spawned.Length; i++) {
+				if (spawned [i]) {
+					print(spawned[i].transform.name);
+					Destroy (spawned [i]);
+				}
+			}
+			SpawnCards();
+		}
     }
 
 
     void SpawnCards()
     {
         randomInt = Random.Range(0, spawnees.Length);
-        Instantiate(spawnees[randomInt], spawnPos1.position, spawnPos1.rotation);
+		GameObject cardDealer = Instantiate(spawnees[randomInt], spawnPos1.position, spawnees[randomInt].transform.rotation) as GameObject;
+		spawned [0] = cardDealer;
+
         randomInt = Random.Range(0, spawnees.Length);
-        Instantiate(spawnees[randomInt], spawnPos2.position, spawnPos2.rotation);
-        randomInt = Random.Range(0, spawnees.Length);
-        Instantiate(spawnees[randomInt], spawnPos3.position, spawnPos3.rotation);
+		GameObject cardPlayer = Instantiate(spawnees[randomInt], spawnPos2.position, spawnees[randomInt].transform.rotation) as GameObject;
+		spawned [1] = cardPlayer;
+
+		if (cardPlayer.GetComponent<CardValue>().cardValue > cardDealer.GetComponent<CardValue>().cardValue) {
+			winPhrase.text = "You Win";
+			print ("You Lose");
+		} else {
+			winPhrase.text = "You Lose";
+			print ("You Win");
+		}
     }
 }
