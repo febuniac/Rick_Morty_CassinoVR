785b71457a3d6a58cf02c22021d7d448c2436351
Rafael Ismael <rafaelmismael@gmail.com>
2017-10-19 09:33:34 -0200

    as

---
+++ PokerVR/Assets/Scripts/BetArea.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BetArea : MonoBehaviour {
+	public GameObject[] chips;
+	public TextMesh betText;
+	private int betValue;
+	private Transform[] chipsPos;
+
+	// Use this for initialization
+	void Start () {
+		betValue = 0;
+		chipsPos = new Transform[4];
+		for (int i = 0; i < chipsPos.Length; i++) {
+			chipsPos [i] = chips [i].transform;
+		}
+	}
+	
+
+	private void OnTriggerEnter(Collider col){
+		if (col.transform.name == "Chip") {
+			betValue += 10;
+			betText.text = "Your Bet:" + betValue;
+			Instantiate (chips[3], chipsPos[3].position, chipsPos[3].rotation);
+		}
+	}
+
+	private void OnTriggerStay(Collider col){
+
+	}
+
+	private void OnTriggerExit(Collider col){
+
+	}
+}
---
+++ PokerVR/Assets/Scripts/ChipBet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChipBet : MonoBehaviour {
+
+	public GameObject[] chips;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
